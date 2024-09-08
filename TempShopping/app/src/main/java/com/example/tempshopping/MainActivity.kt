4559a634d7879b0e441f02675c30ceaaae3aae5b
Aryan Chauhan <aryanc206242@gmail.com>
2024-09-08 16:35:22 +0530

    The Shopping List

---
+++ TempShopping/app/src/main/java/com/example/tempshopping/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.tempshopping
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tempshopping.ui.theme.TempShoppingTheme
+import eu.tutorials.theshoppinnglist.ShoppingListApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TempShoppingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ShoppingListApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TempShoppingTheme {
+        Greeting("Android")
+    }
+}
